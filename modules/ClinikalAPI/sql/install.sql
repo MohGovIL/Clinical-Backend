bbdb1a8433c24c540155188f8bde009044d26a0d
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-04-30 11:58:42 +0300

    remove ACL

--- modules/ClinikalAPI/sql/install.sql
+++ modules/ClinikalAPI/sql/install.sql
@@ -21,7 +21,7 @@ REPLACE INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default
 
 
 INSERT INTO `list_options` (`list_id`, `option_id`, `title`, `seq`, `is_default`, `option_value`, `mapping`, `notes`, `codes`, `toggle_setting_1`, `toggle_setting_2`, `activity`, `subtype`, `edit_options`, `timestamp`) VALUES ('apps', 'react-dev', '../client-app/dev-mode/build', '100', '1', '0', '', NULL, '', '0', '0', '1', '', '1', '2017-07-23 09:33:02');
-UPDATE globals SET gl_value = 'style_clinikal_generic.css' WHERE gl_name = 'css_header';
+-- UPDATE globals SET gl_value = 'style_clinikal_generic.css' WHERE gl_name = 'css_header';
 UPDATE globals SET gl_value = '1' WHERE gl_name = 'rest_api';
 
 
