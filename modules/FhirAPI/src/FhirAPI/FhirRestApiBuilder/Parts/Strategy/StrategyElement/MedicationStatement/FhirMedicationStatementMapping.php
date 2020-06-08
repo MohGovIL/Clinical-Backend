fe16d0f3630a22dd91166938a4960a49f896d443
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-06-08 14:21:37 +0300

    PC-1032

--- modules/FhirAPI/src/FhirAPI/FhirRestApiBuilder/Parts/Strategy/StrategyElement/MedicationStatement/FhirMedicationStatementMapping.php
+++ modules/FhirAPI/src/FhirAPI/FhirRestApiBuilder/Parts/Strategy/StrategyElement/MedicationStatement/FhirMedicationStatementMapping.php
@@ -111,50 +111,50 @@ class FhirMedicationStatementMapping extends FhirBaseMapping  implements Mapping
     {
         $dbMedicationStatement = array();
 
-        $dbMedicationStatement['id']=$FHIRMedicationStatement->getId()->getValue();
+        $dbMedicationStatement['id'] = $FHIRMedicationStatement->getId()->getValue();
 
-        $outcomeList=array_flip($this->getOutcomeTypes());
-        $outcomeCoding= $FHIRMedicationStatement->getStatus();
-        $outcome= $outcomeCoding->getValue();
-        $outcomeId=$outcomeList[$outcome];
+        $outcomeList = array_flip($this->getOutcomeTypes());
+        $outcomeCoding = $FHIRMedicationStatement->getStatus();
+        $outcome = $outcomeCoding->getValue();
+        $outcomeId = $outcomeList[$outcome];
 
-        if(!is_null($outcome) && is_null($outcomeId)){
-             ErrorCodes::http_response_code('400','outcome error');
-             return;
-        }else{
-            $medicationStatementDataFromDb['outcome']=$outcomeId;
+        if (!is_null($outcome) && is_null($outcomeId)) {
+            ErrorCodes::http_response_code('400', 'outcome error');
+            return;
+        } else {
+            $medicationStatementDataFromDb['outcome'] = $outcomeId;
         }
 
-        $categoryCoding= $FHIRMedicationStatement->getCategory()->getCoding()[0];
-        $medicationStatementDataFromDb['list_option_id']=$categoryCoding->getCode()->getValue();
+        $categoryCoding = $FHIRMedicationStatement->getCategory()->getCoding()[0];
+        $medicationStatementDataFromDb['list_option_id'] = $categoryCoding->getCode()->getValue();
 
-        if(!is_null($medicationStatementDataFromDb['list_option_id'])){
-            $type=$categoryCoding->getSystem()->getValue();
-            $medicationStatementDataFromDb['type']=substr($type, strrpos($type, '/') + 1);
-        }else{
-            $medicationStatementDataFromDb['type']=null;
+        if (!is_null($medicationStatementDataFromDb['list_option_id'])) {
+            $type = $categoryCoding->getSystem()->getValue();
+            $medicationStatementDataFromDb['type'] = substr($type, strrpos($type, '/') + 1);
+        } else {
+            $medicationStatementDataFromDb['type'] = null;
         }
-        $medicationStatementDataFromDb['title']= $FHIRMedicationStatement->getCategory()->getText()->getValue();
+        $medicationStatementDataFromDb['title'] = $FHIRMedicationStatement->getCategory()->getText()->getValue();
 
-        $period=$FHIRMedicationStatement->getEffectivePeriod();
+        $period = $FHIRMedicationStatement->getEffectivePeriod();
 
         $medicationStatementDataFromDb['begdate'] = $period->getStart();
 
         $medicationStatementDataFromDb['enddate'] = $period->getEnd();
 
-        $medicationStatementDataFromDb['date'] =  $FHIRMedicationStatement->getDateAsserted();
+        $medicationStatementDataFromDb['date'] = $FHIRMedicationStatement->getDateAsserted();
 
-        $code= $FHIRMedicationStatement->getMedicationCodeableConcept()->getCoding()[0];
+        $code = $FHIRMedicationStatement->getMedicationCodeableConcept()->getCoding()[0];
         $medicationCode = $code->getCode()->getValue();
-        $medicationSystem =  $code->getSystem()->getValue();
-        if(!is_null($medicationCode) && !is_null($medicationSystem)){
-            $medicationStatementDataFromDb['diagnosis']=$medicationSystem.":".$medicationCode;
-        }else{
-            $medicationStatementDataFromDb['diagnosis']=null;
+        $medicationSystem = $code->getSystem()->getValue();
+        if (!is_null($medicationCode) && !is_null($medicationSystem)) {
+            $medicationStatementDataFromDb['diagnosis'] = $medicationSystem . ":" . $medicationCode;
+        } else {
+            $medicationStatementDataFromDb['diagnosis'] = null;
         }
 
-        $userRef =  $FHIRMedicationStatement->getInformationSource()->getReference()->getValue();
-        if(!is_null($userRef) && $userRef!==""){
+        $userRef = $FHIRMedicationStatement->getInformationSource()->getReference()->getValue();
+        if (!is_null($userRef) && $userRef !== "") {
             $medicationStatementDataFromDb['user'] = substr($userRef, strrpos($userRef, '/') + 1);
         }
 
@@ -174,138 +174,72 @@ class FhirMedicationStatementMapping extends FhirBaseMapping  implements Mapping
     {
         $medicationStatementDataFromDb = $params[0];
 
-        $FHIRMedicationStatement =$this->FHIRMedicationStatement;
+        $FHIRMedicationStatement = $this->FHIRMedicationStatement;
         $FHIRMedicationStatement->getId()->setValue($medicationStatementDataFromDb['id']);
 
-        if(!is_null($medicationStatementDataFromDb['outcome']) && $medicationStatementDataFromDb['outcome'] !=="" ){
-            $outcomeList=$this->getOutcomeTypes();
-            $outcome=$outcomeList[$medicationStatementDataFromDb['outcome']];
-            $outcomeCoding= $FHIRMedicationStatement->getStatus();
+        if (!is_null($medicationStatementDataFromDb['outcome']) && $medicationStatementDataFromDb['outcome'] !== "") {
+            $outcomeList = $this->getOutcomeTypes();
+            $outcome = $outcomeList[$medicationStatementDataFromDb['outcome']];
+            $outcomeCoding = $FHIRMedicationStatement->getStatus();
             $outcomeCoding->setId($medicationStatementDataFromDb['outcome']);
             $outcomeCoding->setValue($outcome);
         }
 
-        $categoryCoding= $FHIRMedicationStatement->getCategory()->getCoding()[0];
+        $categoryCoding = $FHIRMedicationStatement->getCategory()->getCoding()[0];
 
-        if(!is_null($medicationStatementDataFromDb['list_option_id'])){
+        if (!is_null($medicationStatementDataFromDb['list_option_id'])) {
             $categoryCoding->getCode()->setValue($medicationStatementDataFromDb['list_option_id']);
-            $categoryCoding->getSystem()->setValue("clinikal/medicationStatement/category/".$medicationStatementDataFromDb['type']);
+            $categoryCoding->getSystem()->setValue("clinikal/medicationStatement/category/" . $medicationStatementDataFromDb['type']);
         }
 
-        if(!is_null($medicationStatementDataFromDb['title'])){
+        if (!is_null($medicationStatementDataFromDb['title'])) {
             $FHIRMedicationStatement->getCategory()->getText()->setValue($medicationStatementDataFromDb['title']);
         }
 
-        $period=$FHIRMedicationStatement->getEffectivePeriod();
+        $period = $FHIRMedicationStatement->getEffectivePeriod();
 
-        if(!is_null($medicationStatementDataFromDb['begdate'])){
-            $date=$this->createFHIRDateTime($medicationStatementDataFromDb['begdate']);
+        if (!is_null($medicationStatementDataFromDb['begdate'])) {
+            $date = $this->createFHIRDateTime($medicationStatementDataFromDb['begdate']);
             $period->setStart($date);
         }
 
-        if(!is_null($medicationStatementDataFromDb['enddate'])){
-            $date=$this->createFHIRDateTime($medicationStatementDataFromDb['enddate']);
+        if (!is_null($medicationStatementDataFromDb['enddate'])) {
+            $date = $this->createFHIRDateTime($medicationStatementDataFromDb['enddate']);
             $period->setEnd($date);
         }
 
-        if(!is_null($medicationStatementDataFromDb['date'])){
-            $date=$this->createFHIRDateTime($medicationStatementDataFromDb['date']);
+        if (!is_null($medicationStatementDataFromDb['date'])) {
+            $date = $this->createFHIRDateTime($medicationStatementDataFromDb['date']);
             $FHIRMedicationStatement->setDateAsserted($date);
         }
 
-        $codeFromDb=explode(":",$medicationStatementDataFromDb['diagnosis']);
+        $codeFromDb = explode(":", $medicationStatementDataFromDb['diagnosis']);
 
-        if(count($codeFromDb)>1){
-            $code= $FHIRMedicationStatement->getMedicationCodeableConcept()->getCoding()[0];
+        if (count($codeFromDb) > 1) {
+            $code = $FHIRMedicationStatement->getMedicationCodeableConcept()->getCoding()[0];
             $code->getCode()->setValue($codeFromDb[1]);
-            $code->getSystem()->setValue(self::LIST_SYSTEM_LINK.$codeFromDb[0]);
+            $code->getSystem()->setValue(self::LIST_SYSTEM_LINK . $codeFromDb[0]);
         }
 
-        if(!empty($medicationStatementDataFromDb['pid'])){
-            $FHIRMedicationStatement->getSubject()->getReference()->setValue("Patient/".$medicationStatementDataFromDb['pid']) ;
+        if (!empty($medicationStatementDataFromDb['pid'])) {
+            $FHIRMedicationStatement->getSubject()->getReference()->setValue("Patient/" . $medicationStatementDataFromDb['pid']);
         }
 
-        if(!empty($medicationStatementDataFromDb['user'])){
-            $FHIRMedicationStatement->getInformationSource()->getReference()->setValue("Practitioner/".$medicationStatementDataFromDb['user']);
+        if (!empty($medicationStatementDataFromDb['user'])) {
+            $FHIRMedicationStatement->getInformationSource()->getReference()->setValue("Practitioner/" . $medicationStatementDataFromDb['user']);
         }
 
         $FHIRMedicationStatement->getNote()[0]->setText($medicationStatementDataFromDb['comments']);
 
-        $this->FHIRMedicationStatement=$FHIRMedicationStatement;
+        $this->FHIRMedicationStatement = $FHIRMedicationStatement;
 
         return $FHIRMedicationStatement;
     }
 
     public function parsedJsonToDb($parsedData)
     {
-        $dbPatient = array();
-        if($parsedData['resourceType']!=="Patient"){
-            return $dbPatient;
-        }
-
-        $dbPatient['pid'] = (is_null($parsedData['id'])) ? null : ucfirst($parsedData['id']);
-        $dbPatient['ss'] = (empty($parsedData['identifier'])) ? null :$parsedData['identifier'][0]['value'];
-        $dbPatient['sex'] = (is_null($parsedData['gender'])) ? null : ucfirst($parsedData['gender']);
-        $dbPatient['DOB'] = (is_null($parsedData['birthDate'])) ? null :$parsedData['birthDate'];
-        $dbPatient['deceased_date'] = (is_null($parsedData['deceasedDateTime'])) ? null : substr($parsedData['deceasedDateTime'],0,10);
-
-        $patientName = $parsedData['name'][0];
-        $dbPatient['lname'] = (is_null($patientName['family'])) ? null : $patientName['family'];
-
-        $dbPatient['fname'] = (is_null($patientName['given'][0])) ? null : $patientName['given'][0];
-        unset($patientName['given'][0]);
-        $dbPatient['mname'] = (empty($patientName['given'])) ? null : implode(" ",$patientName['given']);
-
-        $mainAddress = $parsedData['address'][0];
-
-        if(!empty($mainAddress['line'])) {
-            $addressType = $mainAddress['type'];
-
-            if ($addressType === "postal" || $addressType === "both") {
-                $dbPatient['street'] =$mainAddress['line'][0];
-                $dbPatient['mh_house_no'] =$mainAddress['line'][1];
-                if($addressType === "both"){
-                    $dbPatient['mh_pobox'] =$mainAddress['line'][2];
-                }
-            } elseif ($addressType === "physical") {
-                $dbPatient['mh_pobox'] =$mainAddress['line'][0];
-            }
-        }
-        $dbPatient['postal_code'] = (is_null($mainAddress['postalCode'])) ? null : $mainAddress['postalCode'];
-        $dbPatient['city'] = (is_null($mainAddress['city'])) ? null : $mainAddress['city'];
-        $dbPatient['country_code'] = (is_null($mainAddress['country'])) ? null : $mainAddress['country'];
-
-        $telecom = $parsedData['telecom'];
-
-        if (!is_null($telecom) && is_array($telecom)) {
-
-            foreach ($telecom as $index => $element) {
-
-                $systemVal = $element['system'];
-                $typeVal = $element['use'];
-
-                if ($systemVal === "phone" && $typeVal === "home") {
-                    $dbPatient['phone_home'] = $element['value'];
-                    continue;
-                }
-                if ($systemVal === "phone" && $typeVal === "mobile") {
-                    $dbPatient['phone_cell'] = $element['value'];
-                    continue;
-                }
-                if ($systemVal === "email") {
-                    $dbPatient['email'] = $element['value'];
-                    continue;
-                }
-            }
-
-        } else {
-            $dbPatient['email'] = null;
-            $dbPatient['phone_home'] = null;
-            $dbPatient['phone_cell'] = null;
-        }
-
-
-        return $dbPatient;
+        $dbMedicationStatement = array();
+        return $dbMedicationStatement;
     }
 
     public function validateDb($data){
@@ -348,13 +282,9 @@ class FhirMedicationStatementMapping extends FhirBaseMapping  implements Mapping
     }
 
     public function parsedJsonToFHIR($data)
-
     {
         $FHIRMedicationStatement =$this->FHIRMedicationStatement;
-
-
         $this->FHIRMedicationStatement=$FHIRMedicationStatement;
-
         return $FHIRMedicationStatement;
     }
 
