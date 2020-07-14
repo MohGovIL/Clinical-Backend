79e61ff402b751d15001013313d0a26f5168f139
eyalvo-matrix <eyalvo.matrix@gmail.com>
2020-07-14 13:35:22 +0300

    PC-1388

--- modules/FhirAPI/src/FhirAPI/FhirRestApiBuilder/Parts/Strategy/StrategyElement/ServiceRequest/FhirServiceRequestMapping.php
+++ modules/FhirAPI/src/FhirAPI/FhirRestApiBuilder/Parts/Strategy/StrategyElement/ServiceRequest/FhirServiceRequestMapping.php
@@ -7,7 +7,10 @@
 
 namespace FhirAPI\FhirRestApiBuilder\Parts\Strategy\StrategyElement\ServiceRequest;
 
+use GenericTools\Model\ListsTable;
 use OpenEMR\FHIR\R4\FHIRDomainResource\FHIRServiceRequest;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRRequestIntent;
+use OpenEMR\FHIR\R4\FHIRElement\FHIRRequestStatus;
 use function DeepCopy\deep_copy;
 use Exception;
 use FhirAPI\FhirRestApiBuilder\Parts\Strategy\StrategyElement\MappingData;
@@ -29,7 +32,17 @@ class FhirServiceRequestMapping extends FhirBaseMapping implements MappingData
     private $adapter = null;
     private $container = null;
     private $FHIRServiceRequest = null;
-    private $formQuestionMapping;
+
+
+    private $xRayList = array();
+    private $medicineList = array();
+    private $codeList = array();
+    private $categoryList = array();
+
+    CONST ORDER_DETAIL_SYSTEM_XRAY = "details_x_ray";
+    CONST ORDER_DETAIL_SYSTEM_MEDICINE = "details_providing_medicine";
+    CONST CODE_SYSTEM = "tests_and_treatments";
+    CONST CATEGORY_SYSTEM = "service_types";
 
     public function __construct(ContainerInterface $container)
     {
@@ -37,16 +50,76 @@ class FhirServiceRequestMapping extends FhirBaseMapping implements MappingData
         $this->container = $container;
         $this->adapter = $container->get('Laminas\Db\Adapter\Adapter');
         $this->FHIRServiceRequest = new FHIRServiceRequest;
+
+        $ListsTable = $this->container->get(ListsTable::class);
+
+        $listXRay = $ListsTable->getListNormalized(self::ORDER_DETAIL_SYSTEM_XRAY);
+        $this->setXRayList($listXRay);
+
+        $listMedicine = $ListsTable->getListNormalized(self::ORDER_DETAIL_SYSTEM_MEDICINE);
+        $this->setMedicineList($listMedicine);
+
+        $listCode = $ListsTable->getListNormalized(self::CODE_SYSTEM);
+        $this->setCodeList($listCode);
+
+        $listCategory = $ListsTable->getListNormalized(self::CATEGORY_SYSTEM);
+        $this->setCategoryList($listCategory);
     }
+
+    /*****************************************get lists*******************************************************/
+
+    public function setXRayList($types)
+    {
+        $this->xRayList=$types;
+        return $this->xRayList;
+    }
+
+    public function getXRayList()
+    {
+        return $this->xRayList;
+    }
+
+    public function setMedicineList($types)
+    {
+        $this->medicineList=$types;
+        return $this->medicineList;
+    }
+
+    public function getMedicineList()
+    {
+        return $this->medicineList;
+    }
+
+    public function setCodeList($types)
+    {
+        $this->codeList=$types;
+        return $this->codeList;
+    }
+
+    public function getCodeList()
+    {
+        return $this->codeList;
+    }
+
+
+    public function setCategoryList($types)
+    {
+        $this->categoryList=$types;
+        return $this->categoryList;
+    }
+
+    public function getCategoryList()
+    {
+        return $this->loincCodes;
+    }
+
+
+    /********************************************************************************************************/
+
 
     public function fhirToDb($FhirObject)
     {
         return array();
-    }
-
-    public function getFormQuestionMapping()
-    {
-        return $this->formQuestionMapping;
     }
 
 
@@ -55,7 +128,30 @@ class FhirServiceRequestMapping extends FhirBaseMapping implements MappingData
         $FHIRServiceRequest = new FHIRServiceRequest;
 
         $FhirId = $this->createFHIRId(null);
+        $FHIRCodeableConcept = $this->createFHIRCodeableConcept(array("code"=>null,"text"=>"","system"=>""));
+        $FHIRReference = $this->createFHIRReference(array("reference"=>null));
+        $FHIRString = $this->createFHIRString(null);
+        $FHIRDateTime =  $this->createFHIRDateTime(null);
+        $FHIRRequestStatus = $this->createFHIRRequestStatus(null);
+        $FHIRRequestIntent = $this->createFHIRRequestIntent(null);
+        $FHIRAnnotation = $this->createFHIRAnnotation(array());
+
         $FHIRServiceRequest->setId($FhirId);
+        $FHIRServiceRequest->addCategory(deep_copy($FHIRCodeableConcept));
+        $FHIRServiceRequest->setEncounter(deep_copy($FHIRReference));
+        $FHIRServiceRequest->addReasonCode(deep_copy($FHIRCodeableConcept));
+        $FHIRServiceRequest->setSubject(deep_copy($FHIRReference));
+        $FHIRServiceRequest->setCode(deep_copy($FHIRCodeableConcept));
+        $FHIRServiceRequest->addOrderDetail(deep_copy($FHIRCodeableConcept));
+        $FHIRServiceRequest->setPatientInstruction(deep_copy($FHIRString));
+        $FHIRServiceRequest->setRequester(deep_copy($FHIRReference));
+        $FHIRServiceRequest->setAuthoredOn(deep_copy($FHIRDateTime));
+        $FHIRServiceRequest->setStatus($FHIRRequestStatus);
+        $FHIRServiceRequest->setIntent($FHIRRequestIntent);
+        $FHIRServiceRequest->addNote($FHIRAnnotation);
+        $FHIRServiceRequest->addPerformer(deep_copy($FHIRReference));
+        $FHIRServiceRequest->setOccurrenceDateTime(deep_copy($FHIRDateTime));
+        $FHIRServiceRequest->addReasonReference(deep_copy($FHIRReference));
 
         $this->FHIRServiceRequest=$FHIRServiceRequest;
         return $FHIRServiceRequest;
@@ -79,20 +175,80 @@ class FhirServiceRequestMapping extends FhirBaseMapping implements MappingData
         }
         $FHIRServiceRequest=$this->FHIRServiceRequest;
 
-        $FHIRServiceRequest->setId($ServiceRequestFromDb['id']);
 
+        $FHIRServiceRequest->getId()->setValue($ServiceRequestFromDb['id']);
+
+
+        if(!is_null($ServiceRequestFromDb['category'])){
+
+            $categoryCoding=$FHIRServiceRequest->getCategory()[0];
+            $serviceRequestCoding=$categoryCoding->getCoding()[0];
+            $categoryList=$this->getCategoryList();
+
+            $categoryCoding->getText()->setValue($categoryList[$ServiceRequestFromDb['instruction_code']]);
+            $serviceRequestCoding->getCode()->setValue($ServiceRequestFromDb['category']);
+            $serviceRequestCoding->getSystem()->setValue(self::LIST_SYSTEM_LINK.self::CATEGORY_SYSTEM);
+        }
+
+
+        $refEncounter= self::ENCOUNTER_URI.$ServiceRequestFromDb['encounter'];
+        $FHIRServiceRequest->getEncounter()->getReference()->setValue($refEncounter);
+
+        $FHIRServiceRequest->getReasonCode()[0]->getCoding()[0]->setCode($ServiceRequestFromDb['reason_code']);
+
+        $refSubject= self::PATIENT_URI.$ServiceRequestFromDb['patient'];
+        $FHIRServiceRequest->getSubject()->getReference()->setValue($refSubject);
+
+        if(!is_null($ServiceRequestFromDb['instruction_code'])) {
+
+            $codeCode=$FHIRServiceRequest->getCode();
+            $codeCoding = $codeCode->getCoding()[0];
+            $codeList=$this->getCodeList();
+
+            $codeCode->getText()->setValue($codeList[$ServiceRequestFromDb['instruction_code']]);
+            $codeCoding->getCode()->setValue($ServiceRequestFromDb['instruction_code']);
+            $codeCoding->getSystem()->setValue(self::LIST_SYSTEM_LINK.self::CODE_SYSTEM);
+
+        }
+
+        $orderDetail=$FHIRServiceRequest->getOrderDetail()[0]->getCoding()[0];
+
+
+        $orderDetail->setCode($ServiceRequestFromDb['order_detail_code']);
+        $orderDetail->getSystem()->setValue(self::LIST_SYSTEM_LINK.$ServiceRequestFromDb['order_detail_system']);
+
+        $FHIRServiceRequest->getPatientInstruction()->setValue($ServiceRequestFromDb['patient_instruction']);
+
+        $refReference = self::PRACTITIONER_URI. $ServiceRequestFromDb['requester'];
+        $FHIRServiceRequest->getRequester()->getReference()->setValue($refReference);
+
+        $authoredOn = $this->createFHIRDateTime($ServiceRequestFromDb['authored_on']);
+        $FHIRServiceRequest->setAuthoredOn($authoredOn);
+
+        $FHIRServiceRequest->getStatus()->setValue($ServiceRequestFromDb['status']);
+
+        $FHIRServiceRequest->getIntent()->setValue($ServiceRequestFromDb['intent']);
+
+        $FHIRServiceRequest->getNote()[0]->setText($ServiceRequestFromDb['note']);
+
+        $refPerformer=self::PRACTITIONER_URI. $ServiceRequestFromDb['performer'];
+        $FHIRServiceRequest->getPerformer()[0]->getReference()->setValue($refPerformer);
+
+        $occurrenceDateTime = $this->createFHIRDateTime($ServiceRequestFromDb['occurrence']);
+        $FHIRServiceRequest->setOccurrenceDateTime($occurrenceDateTime);
+
+        $refReasonReference=self::DOCUMENT_REFERENCE_URI. $ServiceRequestFromDb['reason_reference_doc_id'];
+        $FHIRServiceRequest->getReasonReference()[0]->getReference()->setValue($refReasonReference);
 
         return $FHIRServiceRequest;
 
 
     }
 
-
     public function validateDb($data)
     {
         return true;
     }
-
 
     public function parsedJsonToDb($parsedData)
     {
@@ -116,6 +272,30 @@ class FhirServiceRequestMapping extends FhirBaseMapping implements MappingData
     {
         return array();
     }
+
+    /**
+     * @param $value string
+     * @return FHIRRequestStatus
+     */
+    private function createFHIRRequestStatus($value)
+    {
+        $FHIRRequestStatus = new FHIRRequestStatus;
+        $FHIRRequestStatus->setValue($value);
+        return $FHIRRequestStatus;
+    }
+
+    /**
+     * @param $value string
+     * @return FHIRRequestIntent
+     */
+    private function createFHIRRequestIntent($value)
+    {
+        $FHIRRequestIntent = new FHIRRequestIntent;
+        $FHIRRequestIntent->setValue($value);
+        return $FHIRRequestIntent;
+    }
+
+
 
 
 }
